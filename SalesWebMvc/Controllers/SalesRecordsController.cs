ad8b33eacfe36f18f76506ff212acf07372eb910
Guilherme Reif <guilhermereif10@gmail.com>
2026-01-07 18:08:15 -0300

    Projeto Finalizado

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -7,7 +7,7 @@ namespace SalesWebMvc.Controllers {
         private readonly SalesRecordService _salesRecordService;
 
         public SalesRecordsController(SalesRecordService salesRecordService) {
-            salesRecordService = _salesRecordService;
+            _salesRecordService = salesRecordService;
         }
 
         public IActionResult Index() {
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Controllers {
             if (!maxDate.HasValue) {
                 maxDate = DateTime.Now;
             }
+
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
@@ -30,8 +31,21 @@ namespace SalesWebMvc.Controllers {
             return View(result);
         }
 
-        public async Task<IActionResult> GroupingSearch() {
-            return View();
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate) {
+
+            if (!minDate.HasValue) {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue) {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
+            
         }
     }
 }
