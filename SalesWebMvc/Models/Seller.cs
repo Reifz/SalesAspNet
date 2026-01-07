735276399e2e9b3f1997818408bd3f39795c9843
Guilherme Reif <guilhermereif10@gmail.com>
2026-01-07 12:06:03 -0300

    Other Entities

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,42 @@
+﻿using SalesWebMvc.Migrations;
+using System.Linq;
+
+namespace SalesWebMvc.Models {
+    public class Seller {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        
+        
+        public Seller() {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department) {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr) {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr) {
+            Sales.Remove(sr);
+        } 
+
+        public double TotalSales(DateTime inital, DateTime final) {
+            return Sales.Where(sr => sr.Date >= inital && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+
+
+    }
+}
