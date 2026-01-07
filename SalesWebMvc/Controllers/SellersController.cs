05e49a71fd28c306bde2553e0b0e3f65baed3566
Guilherme Reif <guilhermereif10@gmail.com>
2026-01-07 14:58:39 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -52,6 +52,19 @@ namespace SalesWebMvc.Controllers {
 
         }
 
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null) {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
 
     }
 }
