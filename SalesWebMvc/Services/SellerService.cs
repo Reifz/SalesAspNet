4f45115ed981752350f42a126cff462971a9f55e
Guilherme Reif <guilhermereif10@gmail.com>
2026-01-07 13:56:58 -0300

    Foreign key null

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -12,5 +12,12 @@ namespace SalesWebMvc.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj) {
+
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
